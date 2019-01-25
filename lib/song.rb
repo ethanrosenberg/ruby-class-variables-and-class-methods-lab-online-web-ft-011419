10d378d4246bfc88b62fab68a4e782bbfe94a296
Ethan Rosenberg <ethan@ethanrosenberg.com>
2019-01-25 06:31:03 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,11 +2,13 @@ class Song
   attr_accessor :name, :artist, :genre
   
   @@count = 0
+  @@genres = []
   
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
+    @@genres << genre
     @@count += 1
   end
   
@@ -14,6 +16,7 @@ class Song
     @@count
   end
   
+  def self.genres
   
   
   
